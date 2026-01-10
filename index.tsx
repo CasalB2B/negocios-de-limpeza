c9d1218645c9b85f63f7df83c15220f9ab7309d5
CasalB2B <grupocasalb2b@gmail.com>
2026-01-10 00:44:11 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css'; // Estilos Globais
 import App from './App';
 
